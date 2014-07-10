41ec5bd340e07c92774dc6237db73e58b1dc4101
Adam Collins <adc82@case.edu>
2014-07-09 23:59:31 -0500

    sqlite3

--- workstudy/settings/base.py
+++ workstudy/settings/base.py
@@ -71,7 +71,6 @@ WSGI_APPLICATION = 'workstudy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
