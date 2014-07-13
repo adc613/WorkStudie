c9a27e122e47fe8bd2bf17e6fa8040198b75c638
Adam Collins <adc82@case.edu>
2014-07-13 10:25:08 -0400

    maybe..

--- workstudy/urls.py
+++ workstudy/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-#from django.contrib import admin
+from django.contrib import admin
 
 from .views import *
 
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     url(r'^tasks/', include('tasks.urls', namespace='task')),
 
     
-   # url(r'^admin/', include(admin.site.urls)),
+   url(r'^admin/', include(admin.site.urls)),
 )
